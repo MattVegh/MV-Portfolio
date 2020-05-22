9c7da6658f35dd35cbcd1b8dfd98b1af15fdb75d
Matt Vegh <mdv240@gmail.com>
2020-05-22 13:50:00 -0500

    aboutme component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import SideHeader from './components/SideHeader'
+import AboutMe from './components/AboutMe'
 
 function App() {
   return (
@@ -8,18 +9,8 @@ function App() {
       <SideHeader />
 
       <main>
-
-        <section id="aboutme">
-          <h2 class="title forAboutMe">About Me</h2>
-          <p>I'm Currently in Austin, TX by way of Cleveland. I'm a graduate of Thinkful's FullStack Web
-          Developer
-          program.
-          I came from a background of basically only knowing how to turn a computer on, so learning how to code
-          was
-          quite the jump. But as I continue to learn I find that I'm loving the creative freedom coding gives you.
-          I am currently using what I've learned doing freelance and personal work.
-            </p>
-        </section>
+        <AboutMe />
+        
         <section id="portfolio">
           <h2 className="title forPortfolio">Porfolio</h2>
           <div className="projects-container">
