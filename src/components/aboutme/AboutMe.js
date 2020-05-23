94a7e9dbfbe569640940dbc8ebc457ec890ffec5
Matt Vegh <mdv240@gmail.com>
2020-05-22 21:54:45 -0500

    media screen

--- src/components/aboutme/AboutMe.js
+++ src/components/aboutme/AboutMe.js
@@ -5,7 +5,7 @@ export default function AboutMe() {
     return (
         <section id="aboutme">
             <h2 class="about-title">About Me</h2>
-            <p>I'm Currently in Austin, TX by way of Cleveland. I'm a graduate of Thinkful's FullStack Web
+            <p className='about-p'>I'm Currently in Austin, TX by way of Cleveland. I'm a graduate of Thinkful's FullStack Web
             Developer
             program.
             I came from a background of basically only knowing how to turn a computer on, so learning how to code
