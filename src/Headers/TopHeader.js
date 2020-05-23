fe68a6ed3f1a37157787596aaaedb8dc678c3f9e
Matt Vegh <mdv240@gmail.com>
2020-05-22 21:49:41 -0500

    headers done

--- src/Headers/TopHeader.js
+++ src/Headers/TopHeader.js
@@ -5,18 +5,17 @@ import './Headers.css'
 export default function TopHeader() {
     return (
         <div className='top-header'>
-                <nav className='nav-buttons'>
-                    <Link className="nav-link" to='/'>About</Link>
-                    <Link className="nav-link" to='/projects'>Portfolio</Link>
-                </nav>
-                <div id="contact-box">
-                    <div className="contactlinks">
-                        <a className="email" href="mailto:mdv240@gmail.com" target="_blank"><i className="fas fa-envelope-square"></i></a>
-                        <a className="github" href="https://github.com/MattVegh" target="_blank"><i className="fab fa-github-square"></i></a>
-                        <a className="linkedin" href="https://www.linkedin.com/in/mattvegh/" target="_blank"><i className="fab fa-linkedin"></i></a>
-                    </div>
+            <nav className='top-nav-buttons'>
+                <div className='top-nav-link-box'>
+                    <Link className="top-nav-link" to='/'>About</Link>
+                    <Link className="top-nav-link" to='/projects'>Portfolio</Link>
                 </div>
-            
+                <div className='top-contactlinks'>
+                    <a className="email" href="mailto:mdv240@gmail.com" target="_blank"><i className="fas fa-envelope-square"></i></a>
+                    <a className="github" href="https://github.com/MattVegh" target="_blank"><i className="fab fa-github-square"></i></a>
+                    <a className="linkedin" href="https://www.linkedin.com/in/mattvegh/" target="_blank"><i className="fab fa-linkedin"></i></a>
+                </div>
+            </nav>
         </div>
     )
 }
