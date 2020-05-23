a70ed80cc6ea3dda406ac3b2bc32c7891586b006
Matt Vegh <mdv240@gmail.com>
2020-05-23 13:20:29 -0500

    aboutme

--- src/components/aboutme/AboutMe.js
+++ src/components/aboutme/AboutMe.js
@@ -6,14 +6,17 @@ export default function AboutMe() {
     return (
         <section id="aboutme">
             <h2 class="about-title">About Me</h2>
-            <p className='about-p'>I'm Currently in Austin, TX by way of Cleveland. I'm a graduate of Thinkful's FullStack Web
-            Developer
-            program.
-            I came from a background of basically only knowing how to turn a computer on, so learning how to code
-            was
-            quite the jump. But as I continue to learn I find that I'm loving the creative freedom coding gives you.
-            I am currently using what I've learned doing freelance and personal work.
+            <p className='about-p'>
+                I'm Currently in Austin, Texas by way of Cleveland. I'm a graduate of Thinkful's FullStack Web
+            Developer program.</p>
+                <p>Tech-wise I came from a background of basically only knowing how to turn a computer on, so learning how to code
+            was quite the jump.
+                Professionally I have years of experience in retail and warehouse management environments, 
+            which has given me a proven track-record of great client relations and an understanding of how to work in a fast-paced environment.
             </p>
+                <p>Now I'm a Full Stack Developer skilled in JavaScript, React, Express, and RESTful API's.</p>
+               
+            
 
             <Link to='/projects' className='link-from-about'>Check out what I've been working on</Link>
         </section>
