96b0299178dc6a7751e4f64e2e90e4e5204c13f3
Matt Vegh <mdv240@gmail.com>
2020-05-26 15:29:15 -0500

    resume added

--- src/components/aboutme/AboutMe.js
+++ src/components/aboutme/AboutMe.js
@@ -6,19 +6,17 @@ export default function AboutMe() {
     return (
         <section id="aboutme">
             <h2 class="about-title">About Me</h2>
-            <p className='about-p'>
+            <p>
                 I'm Currently in Austin, Texas by way of Cleveland. I'm a graduate of Thinkful's FullStack Web
             Developer program.</p>
-                <p>Tech-wise I came from a background of basically only knowing how to turn a computer on, so learning how to code
-            was quite the jump.
-                Professionally I have years of experience in retail and warehouse management environments, 
-            which has given me a proven track-record of great client relations and an understanding of how to work in a fast-paced environment.
-            </p>
-                <p>Now I'm a Full Stack Developer skilled in JavaScript, React, Express, and RESTful API's.</p>
-               
-            
+            <p>I am a Full stack developer skilled in JavaScript, React, HTML and CSS with a love for front-end design and analytical thinking.</p>
+            <p>Background in retail management provided me with a proven track-record of effortless client relations and working well in fast-paced environments. 
+                Excited to join a team of like-minded professionals who enjoy being challenged on a daily basis.  </p>
+
+
 
             <Link to='/projects' className='link-from-about'>Check out what I've been working on</Link>
+            <a href='https://docs.google.com/document/d/1lRcfOrECDg5ur8RaSDmVaHUagvxgyxPiJ1QhnJ01Mq0/edit?usp=sharing' className='link-to-resume' target='_blank'>My Resume</a>
         </section>
     )
 }
