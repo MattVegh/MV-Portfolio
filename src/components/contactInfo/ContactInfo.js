40f13d3bacd699eba55b9039a5dc99c6b4970dcd
Matt Vegh <mdv240@gmail.com>
2020-05-27 15:53:26 -0500

    typo

--- src/components/contactInfo/ContactInfo.js
+++ src/components/contactInfo/ContactInfo.js
@@ -10,7 +10,7 @@ export default function ContactInfo() {
                 <a className="email" href="mailto:mdv240@gmail.com" target="_blank">Email</a>
                 <a className="github" href="https://github.com/MattVegh" target="_blank">GitHub</a>
                 <a className="linkedin" href="https://www.linkedin.com/in/mattvegh/" target="_blank">LinkedIn</a>
-                <a className='telegram' href='https://www.telegramstop.com/' target='_blank'>Telegram <span className='notasponser'>(#notasponser)</span></a>
+                <a className='telegram' href='https://www.telegramstop.com/' target='_blank'>Telegram <span className='notasponsor'>(#notasponsor)</span></a>
             </div>
         </section>
     )
